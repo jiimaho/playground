c3c238674efdf84505d45150ef7a0246506ab08f
Jim Aho <jim@steadycoding.com>
2023-06-05 21:54:05 +0200

    More stuff for Tuples

--- src/dotnet/Tuples/Program.cs
+++ src/dotnet/Tuples/Program.cs
@@ -1,4 +1,6 @@
-﻿var tuple1 = new Tuple<string, int>("Jim", 1);
+﻿Console.WriteLine("Don't bother read output that much. Read the code and comments instead. Everything has a meaning.");
+
+var tuple1 = new Tuple<string, int>("Jim", 1);
 var tuple2 = new Tuple<string, int>("Jim", 1);
 
 // Tuples are reference types
@@ -17,5 +19,11 @@ Console.WriteLine(tuple3 == tuple4); // True
 Console.WriteLine(tuple3.Equals(tuple4)); // True
 
 var valueTuplesCanBeCreatedAsThis = ("Jim", 1);
+valueTuplesCanBeCreatedAsThis.Item1 = "Bob"; // Weird. Works since value tuples are mutable
+
+var thisIsNice = (Name: "Jim", Age: 1); // Nice way of creating a ValueTuple with explicit property names
 
 Console.WriteLine(tuple3.Equals(valueTuplesCanBeCreatedAsThis)); // True
+Console.WriteLine(tuple3.Equals(thisIsNice)); // True
+Console.WriteLine(thisIsNice.GetType()); // System.ValueTuple`2[System.String,System.Int32]
+Console.WriteLine(thisIsNice.Name); // Explicit property name
