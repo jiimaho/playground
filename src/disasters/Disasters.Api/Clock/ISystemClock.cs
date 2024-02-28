b454ec6446d3b18a2d36ae394c40eeaa19f1cd42
Jim Aho <jaho1@volvocars.com>
2024-02-28 09:38:02 +0100

    chore(cleanup): remove unnecessary system clock

--- src/disasters/Disasters.Api/Clock/ISystemClock.cs
+++
@@ -1,7 +0,0 @@
-namespace Disasters.Api.Clock;
-
-public interface ISystemClock
-{
-    DateTimeOffset UtcNow { get; }
-    DateTimeOffset Now => UtcNow.ToLocalTime();
-}
--- src/disasters/Disasters.Api/Clock/SystemClock.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Disasters.Api.Clock;
-
-public class SystemClock : ISystemClock
-{
-    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
-    public DateTimeOffset Now => UtcNow.ToLocalTime();
-}
