36d4099f8986d87e25fea45e135441db6889ddee
Jim Aho <jaho1@volvocars.com>
2024-06-16 20:49:40 +0200

    feat(grpc): add server and client blog

---
+++ src/csharp-grpc-course/BlogClient/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Grpc.Core;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress(new Uri("http://localhost:5285"), new GrpcChannelOptions
+{
+    Credentials = ChannelCredentials.Insecure
+});
+
+var client = new BlogService.BlogServiceClient(channel);
+
+// await client.CreateBlogAsync(new CreateBlogRequest
+//     { Blog = new Blog { Author = "1", Title = "My First Blog", Content = "Content of the blog" } });
+
+// var result = await client.ReadBlogAsync(new ReadBlogRequest { Id = "666f23984172094b7d551103" });
+//
+// Console.WriteLine(result.Blog.Title);
+// Console.WriteLine(result.Blog.Content);
+// Console.WriteLine(result.Blog.Author);
+// Console.WriteLine(result.Blog.Id);
+// Console.WriteLine("Hello, World!");
+
+// await client.UpdateBlogAsync(new UpdateBlogRequest
+// {
+//     Blog = new Blog
+//     {
+//         Author = "jim",
+//         Content = "new content",
+//         Title = "new title",
+//         Id = "666f23984172094b7d551103"
+//     }
+// });
+
+await client.DeleteBlogAsync(new DeleteBlogRequest { Id = "666f23984172094b7d551103" });
--- src/csharp-grpc-course/BlogServer/Services/BlogServiceImpl.cs
+++ src/csharp-grpc-course/BlogServer/Services/BlogServiceImpl.cs
@@ -1,6 +1,113 @@
+using Grpc.Core;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
 namespace BlogServer.Services;
 
-public class BlogServiceImpl
+public class BlogServiceImpl : BlogService.BlogServiceBase
 {
+    private static MongoClient _mongoClient = new MongoClient("mongodb://admin:password@localhost:27017");
+    private static IMongoDatabase _mongoDatabase = _mongoClient.GetDatabase("mydb");
+    private static IMongoCollection<BsonDocument> _mongoCollection = _mongoDatabase.GetCollection<BsonDocument>("blog");
     
+    public override Task<CreateBlogResponse> CreateBlog(CreateBlogRequest request, ServerCallContext context)
+    {
+        var blog = request.Blog;
+        BsonDocument doc = new BsonDocument("author_id", blog.Author)
+            .Add("title", blog.Title)
+            .Add("content", blog.Content);
+
+        _mongoCollection.InsertOne(doc);
+
+        var id = doc.GetValue("_id").ToString();
+
+        blog.Id = id;
+
+        return Task.FromResult(new CreateBlogResponse { Blog = blog });
+    }
+
+    public override async Task<ReadBlogResponse> ReadBlog(ReadBlogRequest request, ServerCallContext context)
+    {
+        var id = request.Id;
+
+        var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(id));
+
+        var result = await _mongoCollection.FindAsync(filter);
+        var single = result.SingleOrDefault();
+        
+        if (single is null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Blog not found"));
+
+        var blog = new Blog
+        {
+            Author = single.GetValue("author_id").ToString(),
+            Title = single.GetValue("title").ToString(),
+            Content = single.GetValue("content").ToString()
+        };
+        
+        return new ReadBlogResponse { Blog = blog };
+    }
+
+    public override async Task<UpdateBlogResponse> UpdateBlog(UpdateBlogRequest request, ServerCallContext context)
+    {
+        var id = request.Blog.Id;
+        
+        var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(id));
+
+        var matches = await _mongoCollection.FindAsync(filter);
+
+        var blog = matches.SingleOrDefault();
+        
+        if (blog is null)
+            throw new RpcException(new Status(StatusCode.NotFound, "Blog not found"));
+        
+        blog.Set("author_id", request.Blog.Author);
+        blog.Set("title", request.Blog.Title);
+        blog.Set("content", request.Blog.Content);
+        
+        await _mongoCollection.ReplaceOneAsync(filter, blog);
+        
+        return new UpdateBlogResponse
+        {
+            Blog = new Blog
+            {
+                Author = request.Blog.Author,
+                Title = request.Blog.Title,
+                Content = request.Blog.Content,
+                Id = request.Blog.Id
+            }
+        };
+    }
+
+    public override async Task<DeleteBlogResponse> DeleteBlog(DeleteBlogRequest request, ServerCallContext context)
+    {
+        var id = request.Id;
+        
+        var filter = new FilterDefinitionBuilder<BsonDocument>().Eq("_id", new ObjectId(id));
+        
+        var result = await _mongoCollection.DeleteOneAsync(filter);
+
+        if (result.DeletedCount == 0)
+            throw new RpcException(new Status(StatusCode.NotFound, "Blog not found"));
+        return new DeleteBlogResponse { Id = id };
+    }
+
+    public override async Task<ListBlogResponse> ListBlog(Empty request, ServerCallContext context)
+    {
+        var filter = new FilterDefinitionBuilder<BsonDocument>().Empty;
+        var result = await _mongoCollection.FindAsync(filter);
+
+        var blogs = result.ToEnumerable().Select(b => new Blog
+        {
+            Author = b.GetValue("author_id").ToString(),
+            Title = b.GetValue("title").ToString(),
+            Content = b.GetValue("content").ToString(),
+            Id = b.GetValue("_id").ToString()
+        });
+
+        return new ListBlogResponse
+        {
+            Blog = { blogs }
+        };
+    }
 }
