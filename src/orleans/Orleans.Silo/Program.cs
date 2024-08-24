70a11dcfb44eeca7f336cbb9c49495cc919b818b
Jim Aho <jaho1@volvocars.com>
2024-08-24 18:42:07 +0200

    feat: enable orleans dynamodb clustering and grain storage with dynamodb
    - To test this locally first run "localstack start" in to start
    localstack. Run the Orleans.Silo project and two tables will be created
    in DynamodDB in the region us-east-1. Very unsure why that region, but
    it works.
    - You should now see some rows in the OrleansSilos DynamoDB table that
    keeps track of the different silos that are in the cluster.
    - When starting ANOTHER silo, make sure GATEWAY_PORT and SILO_PORT and
    SILO_NUMBER are set to unique values.

--- src/orleans/Orleans.Silo/Program.cs
+++ src/orleans/Orleans.Silo/Program.cs
@@ -1,13 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net;
+using Orleans.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseOrleans((ctx, siloBuilder) =>
 {
-    siloBuilder.UseLocalhostClustering();
-    siloBuilder.AddMemoryGrainStorage("memory");
+    siloBuilder.UseDynamoDBClustering(options =>
+    {
+        options.CreateIfNotExists = true;
+        options.Service = "eu-west-1";
+    });
+    // siloBuilder.UseLocalhostClustering();
+    siloBuilder.AddDynamoDBGrainStorage("blazorStore", options =>
+    {
+        options.CreateIfNotExists = true;
+        options.Service = "eu-west-1";
+    });
+
+    siloBuilder.Services.Configure<EndpointOptions>(options => 
+    { 
+        options.AdvertisedIPAddress = IPAddress.Loopback;
+        // options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, 8081);
+        options.GatewayPort = int.Parse(Environment.GetEnvironmentVariable("GATEWAY_PORT"));
+        // options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, 8082);
+        options.SiloPort = int.Parse(Environment.GetEnvironmentVariable("SILO_PORT"));
+    });
+    siloBuilder.Services.Configure<ClusterOptions>(options =>
+    {
+        options.ClusterId = "blazor-cluster";
+        options.ServiceId = "blazor-service";
+    });
+    siloBuilder.Services.Configure<SiloOptions>(options => 
+    {
+        options.SiloName = $"silo-number-{Environment.GetEnvironmentVariable("SILO_NUMBER")}"; 
+    });
+    
     siloBuilder.Services.AddLogging();
-});
+}).UseConsoleLifetime();
 
 var app = builder.Build();
 
