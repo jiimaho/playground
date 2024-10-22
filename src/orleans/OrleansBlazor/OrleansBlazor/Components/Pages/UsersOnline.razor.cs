54a4e223068327670be029ffff4569a8ee7aaf49
Jim Aho <jaho1@volvocars.com>
2024-10-22 10:55:22 +0200

    feat(blazor): set timer to update users online

--- src/orleans/OrleansBlazor/OrleansBlazor/Components/Pages/UsersOnline.razor.cs
+++ src/orleans/OrleansBlazor/OrleansBlazor/Components/Pages/UsersOnline.razor.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Components;
 using Orleans.Silo;
 using Orleans.Silo.Primitives;
+using Timer = System.Timers.Timer;
 
 namespace OrleansBlazor.Components.Pages;
 
@@ -16,6 +17,8 @@ public partial class UsersOnline : ComponentBase
     private List<Username> AllUsersOnline { get; set; } = [];
 
     private IChatRoomObserver _chatRoomObserver = null!;
+
+    private Timer _updateUsersOnline;
 
     protected override Task OnInitializedAsync()
     {
@@ -39,6 +42,16 @@ public partial class UsersOnline : ComponentBase
         _chatRoomObserver = ClusterClient.CreateObjectReference<IChatRoomObserver>(observer);
 
         _ = chatRoomGrain.Join(_chatRoomObserver);
+
+        _updateUsersOnline = new Timer(TimeSpan.FromSeconds(30));
+        _updateUsersOnline.Elapsed += async (sender, args) => {
+            var lastMessageByUser = await chatRoomGrain.GetLastMessageSentByUsers();
+            var online = lastMessageByUser.Where(m => m.Value > DateTimeOffset.Now.AddMinutes(-1)).Select(m => m.Key);
+            AllUsersOnline.Clear();
+            AllUsersOnline.AddRange(online);
+            await InvokeAsync(StateHasChanged);
+        };
+        _updateUsersOnline.Start();
 
         return Task.CompletedTask;
     }
