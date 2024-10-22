eda20e4f64117dc617c55579d3097a6e73c93e88
Jim Aho <jaho1@volvocars.com>
2024-10-22 11:07:51 +0200

    chore(refactor): simplify users online methods and checks

--- src/orleans/OrleansBlazor/OrleansBlazor/Components/Pages/UsersOnline.razor.cs
+++ src/orleans/OrleansBlazor/OrleansBlazor/Components/Pages/UsersOnline.razor.cs
@@ -8,51 +8,45 @@ namespace OrleansBlazor.Components.Pages;
 
 public partial class UsersOnline : ComponentBase
 {
+    // Parameters
     [Parameter, Required]
     public string ChatRoomId { get; set; } = null!;
 
+    // DI
     [Inject]
     private IClusterClient ClusterClient { get; init; } = null!;
 
+    // State
     private List<Username> AllUsersOnline { get; set; } = [];
 
     private IChatRoomObserver _chatRoomObserver = null!;
 
-    private Timer _updateUsersOnline;
+    private Timer _updateUsersOnline = null!;
+
+    private const int UsersOnlineIntervalInSeconds = 5;
+    private const int UserOnlineDefinitionInMinutes = -1;
 
     protected override Task OnInitializedAsync()
     {
         var chatRoomGrain = ClusterClient.GetGrain<IChatRoom>(ChatRoomId);
-        var observer = new ChatRoomObserver(async _ =>
-        {
-            var lastMessageByUser = await chatRoomGrain.GetLastMessageSentByUsers();
-            var online = lastMessageByUser.Where(m => m.Value > DateTimeOffset.Now.AddMinutes(-1)).Select(m => m.Key);
-            AllUsersOnline.Clear();
-            AllUsersOnline.AddRange(online);
-            await InvokeAsync(StateHasChanged);
-        }, async _ =>
-        {
-            var lastMessageByUser = await chatRoomGrain.GetLastMessageSentByUsers();
-            var online = lastMessageByUser.Where(m => m.Value > DateTimeOffset.Now.AddMinutes(1)).Select(m => m.Key);
-            AllUsersOnline.Clear();
-            AllUsersOnline.AddRange(online);
-            await InvokeAsync(StateHasChanged);
-        });
-
+        var observer = new ChatRoomObserver(_ => UpdateUsersOnline(), _ => UpdateUsersOnline());
         _chatRoomObserver = ClusterClient.CreateObjectReference<IChatRoomObserver>(observer);
-
         _ = chatRoomGrain.Join(_chatRoomObserver);
 
-        _updateUsersOnline = new Timer(TimeSpan.FromSeconds(30));
-        _updateUsersOnline.Elapsed += async (sender, args) => {
-            var lastMessageByUser = await chatRoomGrain.GetLastMessageSentByUsers();
-            var online = lastMessageByUser.Where(m => m.Value > DateTimeOffset.Now.AddMinutes(-1)).Select(m => m.Key);
-            AllUsersOnline.Clear();
-            AllUsersOnline.AddRange(online);
-            await InvokeAsync(StateHasChanged);
-        };
+        _updateUsersOnline = new Timer(TimeSpan.FromSeconds(UsersOnlineIntervalInSeconds));
+        _updateUsersOnline.Elapsed += async (_, _) => { await UpdateUsersOnline(); };
         _updateUsersOnline.Start();
 
         return Task.CompletedTask;
+    }
+
+    private async Task UpdateUsersOnline()
+    {
+        var chatRoomGrain = ClusterClient.GetGrain<IChatRoom>(ChatRoomId);
+        var lastMessageByUser = await chatRoomGrain.GetLastMessageSentByUsers();
+        var online = lastMessageByUser.Where(m => m.Value > DateTimeOffset.Now.AddMinutes(UserOnlineDefinitionInMinutes)).Select(m => m.Key);
+        AllUsersOnline.Clear();
+        AllUsersOnline.AddRange(online);
+        await InvokeAsync(StateHasChanged);
     }
 }
