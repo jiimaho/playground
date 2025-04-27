af81660049f51b26837a61cf4b29e72e845b968c
Jim Aho <jim@steadycoding.com>
2025-04-27 17:55:29 +0200

    add new web with mudblazor

--- src/healthy/Healthy.AppHost/Program.cs
+++ src/healthy/Healthy.AppHost/Program.cs
@@ -2,5 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres");
 
+var web = builder.AddProject<Projects.Healthy_Web>("web");
 
 builder.Build().Run();
